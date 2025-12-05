48ad47900412a3b9606e7bd572a627ab48ad3f30
eloycifpaviles <eloy.rodriguez@cifpaviles.net>
2025-12-05 12:35:55 +0100

    index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-echo "<h1>Hola mundo desde Azure</h1>";
-echo "<p>Aplicación web desplegada usando GitHub</p>";
+echo "<!doctype html><html><head><meta charset='utf-8'><title>Hola Mundo Azure</title></head><body>";
+echo "<h1>Hola mundo desde Azure App Service</h1>";
+echo "<p>Alumno: ELOY_RODRIGUEZ</p>";
+echo "<p>App: webapp01-522D</p>";
+echo "</body></html>";
 ?>
